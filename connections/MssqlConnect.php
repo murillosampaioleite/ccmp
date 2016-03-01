db7f73f3438f66f2212add637dc186e4b9120294
Murillo Sampaio Leite <murillosamp4io@gmail.com>
2016-03-01 16:52:38 -0300

    2

--- connections/MssqlConnect.php
+++ connections/MssqlConnect.php
@@ -3,29 +3,30 @@ $server = 'sv-cirdev';
 $username = 'AppUsrCCMP';
 $password = 'AppUsrCCMP';
 $database = 'CIR2000_VALOR';
-$connection = mssql_connect($server, $username, $password);
+$mssConnection = mssql_connect($server, $username, $password)
+	or die("Não foi possível conectar ao servidor de banco de dados");
  
-if($connection != FALSE) { 
+if($mssConnection != FALSE) { 
 	echo "Connected to the database server OK<br />";
 }else {
 	die("Couldn't connect");
 }
  
-if(mssql_select_db($database, $connection)) {
-	echo "Selected $database ok<br />";
-}else {
-	die('Failed to select DB');
-}
+// if(mssql_select_db($database, $mssConnection)) {
+// 	echo "Selected $database ok<br />";
+// }else {
+// 	die('Failed to select DB');
+// }
 
 
 
 
 
-$query_result = mssql_query('SELECT @@VERSION');
-$row = mssql_fetch_array($query_result);
+// $query_result = mssql_query('SELECT @@VERSION');
+// $row = mssql_fetch_array($query_result);
  
-if($row != FALSE) {
-	echo "Version is {$row[0]}<br />";
-}
-mssql_free_result($query_result);
-mssql_close($connection);
+// if($row != FALSE) {
+// 	echo "Version is {$row[0]}<br />";
+// }
+// mssql_free_result($query_result);
+mssql_close($mssConnection);
